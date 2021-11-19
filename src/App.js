737e0229ff1cc1eb136237f2c45cc952ad13a2f4
Ian Del Giudice <ian.pvd@gmail.com>
2021-11-19 05:09:57 -0500

    WIP page layout

--- src/App.js
+++ src/App.js
@@ -51,24 +51,28 @@ class App extends React.Component {
     return (
       <div className="app">
         <Header />
-        <ColorExtract
-          classStatus={this.state.classStatus}
-          colorsList={this.state.colorsList}
-          extractColors={this.extractColors}
-          hasInput={this.state.hasInput}
-          namedColors={this.state.namedColors}
-        />
-        <section className="section">
-          <h2 className="section-heading">About</h2>
-          <p>Paste in your source text, and Color Extract will use regex to find hex colors. The hex codes will be de-duped, and each unique hex code will be displayed as a swatch.</p>
-          <p>A list of CSS variables will also be generated. Any hex codes for colors that are similar will be consolidated under a single color name. The generated stylesheet will have tabs which format the variables using PostCSS, SCSS, SASS or LESS syntax.</p>
-        </section>
-        <section className="section">
-          <h2 className="section-heading">Colophon</h2>
-        </section>
-        <section className="section">
-          <h2 className="section-heading">Credits & Copyright</h2>
-        </section>
+        <main>
+          <ColorExtract
+            classStatus={this.state.classStatus}
+            colorsList={this.state.colorsList}
+            extractColors={this.extractColors}
+            hasInput={this.state.hasInput}
+            namedColors={this.state.namedColors}
+          />
+          <section className="section">
+            <h2 className="section-heading">About</h2>
+            <p>Paste in your source text, and Color Extract will use regex to find hex colors. The hex codes will be de-duped, and each unique hex code will be displayed as a swatch.</p>
+            <p>A list of CSS variables will also be generated. Any hex codes for colors that are similar will be consolidated under a single color name. The generated stylesheet will have tabs which format the variables using PostCSS, SCSS, SASS or LESS syntax.</p>
+          </section>
+        </main>
+        <footer>
+          <section className="section">
+            <h2 className="section-heading">Colophon</h2>
+          </section>
+          <section className="section">
+            <h2 className="section-heading">Credits & Copyright</h2>
+          </section>
+        </footer>
       </div>
     );
   }
